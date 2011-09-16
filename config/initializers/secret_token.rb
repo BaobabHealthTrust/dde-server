67d7a006d2687dede4e5767ed6b6ead731aa75cd
Willem van Kerkhof <willem.van-kerkhof@innoq.com>
2011-09-16 11:04:01 +0200

    Use different secret tokens for each environment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DdeServer::Application.config.secret_token = 'e19844526686e72720c461913a374395a5ee073ee95b229bbbf3f5da1c4a630e8a0bae91f3ad7914d7ad0e97be620b146885f16e0c8df6ca89bd4a95ea2333c3'
+DdeServer::Application.config.secret_token = File.read(Rails.root.join('config', 'environments', "#{Rails.env}.token"))
