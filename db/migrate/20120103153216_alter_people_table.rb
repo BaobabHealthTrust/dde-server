e39794752727eb91b6d7a0886dee1e678b1c35f2
mwatha <mwatha.bwanalie@baobabhealth.org>
2012-01-09 17:20:00 +0200

    adding national_id on the results we sent after a person search

--- db/migrate/20120103153216_alter_people_table.rb
+++ db/migrate/20120103153216_alter_people_table.rb
@@ -1,7 +1,6 @@
 class AlterPeopleTable < ActiveRecord::Migration
   def self.up
     change_table :people do |p|
-      p.string :national_id, :null => true
       p.string :given_name, :null => true
       p.string :family_name, :null => true
       p.string :gender, :null => true
@@ -11,7 +10,6 @@ class AlterPeopleTable < ActiveRecord::Migration
   end
 
   def self.down
-     remove_column :people, :identifier
      remove_column :people, :given_name
      remove_column :people, :family_name
      remove_column :people, :gender
