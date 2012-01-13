207bda38bfbef6788e090055fe983e2aabf9223d
gamphani <dave.phiri@baobabhealth.org>
2012-01-13 11:39:04 +0200

    Added diversion to creation of healthdata patient. May need to make this configurable

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -82,6 +82,11 @@ class PeopleController < ApplicationController
   # POST /people
   # POST /people.xml
   def create
+    @healthdata_patient = MasterPatientRecord.create_healthdata_patient(params)
+    if @healthdata_patient
+      params[:person]["data"]["patient"]["identifiers"]["old_identification_number"] = "#{@healthdata_patient.Site_ID.to_s}#{@healthdata_patient.Pat_ID.to_i.to_s}"
+    end
+
     @person = Person.new(params[:person].merge( 
                          {:creator_site_id => Site.current_id ,
                          :given_name => params[:person]["data"]["names"]["given_name"] ,
