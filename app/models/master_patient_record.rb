0d99cc8fb740ba78ea550196bd3b63a4f1a7b95b
gamphani <dave.phiri@baobabhealth.org>
2012-01-17 13:38:24 +0200

    Re-introduced previously commented lines after rectifying address params

--- app/models/master_patient_record.rb
+++ app/models/master_patient_record.rb
@@ -6,21 +6,21 @@
     def self.create_healthdata_patient(received_params)
       params = received_params["person"]["data"]
       birthdate = params["birthdate"].to_date                                                      
-    patient_hash = {                                                            
+      patient_hash = {                                                            
         "First_Name" => params["names"]["given_name"],                          
         "Last_Name" => params["names"]["family_name"],                          
         "Sex" => params["gender"],                                              
         "Day_Of_Birth" => birthdate.day,                                        
         "Month_Of_Birth" => birthdate.month,                                    
         "Year_Of_Birth" => birthdate.year,                                      
-       # "Birth_TA" => params["addresses"]["address2"],                          
+        "Birth_TA" => "#{params['addresses']['state_province']} / #{params['addresses']['county_district']}",                          
         "Location" => 0,                                                        
-       # "Address" => "#{addresses['state_province']} / #{addresses['county_district']} / #{addresses['city_village']}",
+        "Address" => "#{params['addresses']['city_village']}",
         "Site_ID" => 101,                                                       
         "Pat_ID" => MasterPatientRecord.maximum(:Pat_ID).to_i + 1,              
         "Birth_Date" => birthdate.strftime("%d-%b-%Y"),                 
         "Date_Reg" => Date.today.strftime("%d-%b-%Y")                           
-        }                                                                       
+        }                                                                     
                                                                                 
     #raise patient_hash.to_yaml                                                 
     patient = MasterPatientRecord.create(patient_hash)                          
