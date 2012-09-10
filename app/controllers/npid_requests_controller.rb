ba25fb07217a8e737d1009d5a4ad01981b1efe5f
mwatha <mwatha.bwanalie@baobabhealth.org>
2012-09-10 15:07:46 +0200

    adding a functionality to upload files

--- app/controllers/npid_requests_controller.rb
+++ app/controllers/npid_requests_controller.rb
@@ -71,7 +71,7 @@ class NpidRequestsController < ApplicationController
       batch_info[:check_sum] = file_info[0]
       batch_info[:file_size] = file_info[1]
       batch_info[:file_name] = filename
-      batch_info[:ids] = ids
+      #batch_info[:ids] = ids
       resp = batch_info.to_json
     end
     
@@ -147,5 +147,10 @@ class NpidRequestsController < ApplicationController
     render :text => resp and return
   end
 
+  def file_acknowledgement_transfer
+    RestClient.post('http://localhost:3002/npids/stay_real.txt', 
+      :name_of_file_param => File.read("#{Rails.root}/npids/stay_real.txt"))
+    render :text => "done" and return
+  end
 
 end
