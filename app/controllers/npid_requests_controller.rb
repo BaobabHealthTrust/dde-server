4e29697e41f61618245fc874298635170ef58379
Mwatha Bwanali <mwathabwanali@gmail.com>
2012-11-20 16:20:40 +0200

    merging with master

--- app/controllers/npid_requests_controller.rb
+++ app/controllers/npid_requests_controller.rb
@@ -71,7 +71,7 @@ class NpidRequestsController < ApplicationController
       batch_info[:check_sum] = file_info[0]
       batch_info[:file_size] = file_info[1]
       batch_info[:file_name] = filename
-      #batch_info[:ids] = ids
+      batch_info[:ids] = ids
       resp = batch_info.to_json
     end
     
