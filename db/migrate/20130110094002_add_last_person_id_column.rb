7e95642cc770567fd23b461054d62da66ce8b923
blessings <blessings.mhango@gmail.com>
2013-01-10 12:05:33 +0200

    added migration script for last person id column in syncs table

---
+++ db/migrate/20130110094002_add_last_person_id_column.rb
@@ -0,0 +1,9 @@
+class AddLastPersonIdColumn < ActiveRecord::Migration
+  def self.up
+    add_column :syncs, :last_person_id, :integer,:null => false, :after => :sync_site_id
+  end
+
+  def self.down
+    remove_column :syncs, :last_person_id
+  end
+end
