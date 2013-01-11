3f2f7d3919a9849f5ced4b9ba266c317c442a6fd
blessings <blessings.mhango@gmail.com>
2013-01-11 11:22:15 +0200

    added last updated person id method

--- app/models/sync.rb
+++ app/models/sync.rb
@@ -1,7 +1,11 @@
 class Sync < ActiveRecord::Base
 
   def self.last_updated_date(site_code)
-    self.where(:'sync_site_id' => site_code).maximum("updated_date")
+    self.where(:'sync_site_id' => site_code).maximum(:updated_date)
+  end
+
+  def self.last_updated_person_id(site_code)
+    self.where(:'sync_site_id' => site_code).maximum(:last_person_id)
   end
 
 end
