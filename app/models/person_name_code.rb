263d171e384c23d5b5eb02fda34ea71de05b5441
blessings <blessings.mhango@gmail.com>
2013-01-12 19:35:28 +0200

    updated create name code method in person name code model

--- app/models/person_name_code.rb
+++ app/models/person_name_code.rb
@@ -3,9 +3,16 @@ class PersonNameCode < ActiveRecord::Base
   belongs_to :person, :foreign_key => :id
 
   def self.create_name_code(person)
-    self.create(:person_id => person.id,
+    found = self.find_by_person_id(person.id)
+    if found.blank?
+      self.create(:person_id => person.id,
                 :given_name_code => person.given_name.soundex,
-                :family_name_code => person.family_name.soundex) 
+                :family_name_code => person.family_name.soundex)
+    else
+      found.given_name_code = person.given_name.soundex
+      found.family_name_code = person.family_name.soundex
+      found.save
+    end           
   end
 
   def self.rebuild_person_name_codes
