2caef64a1d5165f674ce8bc42312f80cb81446cc
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-01-18 21:04:36 +0200

    adding a dde search by name functionality

--- app/models/sync.rb
+++ app/models/sync.rb
@@ -1,7 +1,17 @@
 class Sync < ActiveRecord::Base
 
   def self.last_updated_date(site_code)
-    self.where(:'sync_site_id' => site_code).maximum(:updated_date)
+    if Site.proxy?
+      self.where(:'sync_site_id' => site_code).maximum(:updated_date)
+    else
+      dates = []
+      sites = Site.all
+      sites.each do |site|
+        next if site.code == site_code
+        dates << self.where(:'sync_site_id' => site.code).maximum(:updated_date)
+      end
+      dates.sort.first
+    end
   end
 
   def self.last_updated_person_id(site_code)
