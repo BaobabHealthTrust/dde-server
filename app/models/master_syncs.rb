26d8efe6c03de0d571ad667e2f62c1fa2c0ef75a
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-02-12 23:53:46 +0200

    merging

--- app/models/master_syncs.rb
+++ app/models/master_syncs.rb
@@ -1,2 +1,22 @@
-class MasterSyncs < ActiveRecord::Base
+class MasterSync < ActiveRecord::Base
+  def self.last_updated_date(site_code)             
+    date = self.where("site_code <> ? AND created_date IS NOT NULL 
+      AND updated_date IS NOT NULL",site_code).minimum(:created_date)
+    return date unless date.blank?  
+      
+    self.where("site_code = ? AND created_date IS NOT NULL 
+      AND updated_date IS NOT NULL",site_code).maximum(:created_date)
+  end                                                                           
+                                                                                
+  def self.check_for_valid_start_date(site_code)                                      
+    if(self.where("created_date IS NOT NULL AND updated_date IS NULL AND site_code = ?",site_code).first).blank?        
+      self.create(:created_date => DateTime.now(),:site_code => site_code)                                
+    elsif not(self.where("created_date IS NOT NULL AND updated_date IS NOT NULL AND site_code = ?",site_code).last).blank?        
+      date = (self.where("created_date IS NOT NULL AND updated_date 
+        IS NOT NULL AND site_code = ?",site_code)).maximum(:created_date)
+      self.create(:created_date => date, :site_code => site_code)                                
+    else
+      self.create(:created_date => DateTime.now(),:site_code => site_code)                                
+    end                                                                         
+  end
 end
--- app/models/proxy_syncs.rb
+++ app/models/proxy_syncs.rb
@@ -1,2 +1,32 @@
-class ProxySyncs < ActiveRecord::Base
+class ProxySync < ActiveRecord::Base
+
+  def self.last_updated_date
+    date = self.where("start_date IS NOT NULL AND end_date IS NULL")
+    return date.first.start_date unless date.blank?
+    self.where("start_date IS NOT NULL 
+      AND end_date IS NOT NULL").maximum(:end_date)
+  end
+
+  def self.check_for_valid_start_date
+    if not(self.where("start_date IS NOT NULL AND end_date IS NULL")).blank?
+      if(Person.count < 1)
+        self.create(:start_date => DateTime.now())
+      else
+        self.create(:start_date => (Person.where('id > 0').minimum('created_at') - 1.minute))
+      end
+    elsif not(self.where("start_date IS NOT NULL AND end_date IS NOT NULL").last).blank?
+      if(Person.count < 1)
+        self.create(:start_date => DateTime.now())
+      else
+        self.create(:start_date => (Person.where('id > 0').maximum(:updated_at)))
+      end
+    else
+      if(Person.count < 1)
+        self.create(:start_date => DateTime.now())
+      else
+        self.create(:start_date => (Person.where('id > 0').minimum('created_at') - 1.minute))
+      end
+    end
+  end
+
 end
