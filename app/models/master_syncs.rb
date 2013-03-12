02c2a8c2b274fdede87d575c6e9d6825a976c7a2
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-03-12 12:34:04 +0200

    working on sync - managing time

--- app/models/master_syncs.rb
+++ app/models/master_syncs.rb
@@ -1,23 +1,17 @@
 class MasterSyncs < ActiveRecord::Base
 
   def self.last_updated_datetime(site_code)             
-    date = self.where("site_code <> ? AND created_date IS NOT NULL 
-      AND updated_date IS NOT NULL",site_code).minimum(:created_at)
-    return date unless date.blank?  
-      
-    self.where("site_code = ? AND created_date IS NOT NULL 
-      AND updated_date IS NOT NULL",site_code).maximum(:created_at)
+    self.where("site_code <> ? AND created_date IS NOT NULL 
+      AND updated_date IS NOT NULL",site_code).maximum(:updated_at)
   end                                                                           
                                                                                 
   def self.check_for_valid_start_date(site_code)                                      
-    if(self.where("created_date IS NOT NULL AND updated_date IS NULL AND site_code = ?",site_code).first).blank?        
-      self.create(:created_date => DateTime.now(),:site_code => site_code)                                
-    elsif not(self.where("created_date IS NOT NULL AND updated_date IS NOT NULL AND site_code = ?",site_code).last).blank?        
-      date = (self.where("created_date IS NOT NULL AND updated_date 
-        IS NOT NULL AND site_code = ?",site_code)).maximum(:created_at)
-      self.create(:created_date => date, :site_code => site_code)                                
+    if(self.where("created_date IS NOT NULL AND updated_date IS NULL AND site_code = ?",site_code).first)       
+      return true
+    elsif(self.where("created_date IS NOT NULL AND updated_date IS NOT NULL AND site_code = ?",site_code).last)
+      self.create(:created_date => Date.today, :site_code => site_code)                                
     else
-      self.create(:created_date => DateTime.now(),:site_code => site_code)                                
+      self.create(:created_date => Date.today, :site_code => site_code)                                
     end                                                                         
   end
 end
