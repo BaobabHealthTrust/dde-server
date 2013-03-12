3f02ceeae1a63f30b35adfce2ae3230b5b3eb249
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-03-12 14:40:43 +0200

    working on sync - managing time

--- app/models/master_syncs.rb
+++ app/models/master_syncs.rb
@@ -2,7 +2,7 @@ class MasterSyncs < ActiveRecord::Base
 
   def self.last_updated_datetime(site_code)             
     self.where("site_code <> ? AND created_date IS NOT NULL 
-      AND updated_date IS NOT NULL",site_code).maximum(:updated_at)
+      AND updated_date IS NOT NULL",site_code).maximum(:created_at)
   end                                                                           
                                                                                 
   def self.check_for_valid_start_date(site_code)                                      
