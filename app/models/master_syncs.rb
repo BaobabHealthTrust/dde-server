de531596d73a39f85ae087ac2fb4fd55613d2171
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-03-12 16:17:57 +0200

    working on sync - managing time

--- app/models/master_syncs.rb
+++ app/models/master_syncs.rb
@@ -1,8 +1,21 @@
 class MasterSyncs < ActiveRecord::Base
 
   def self.last_updated_datetime(site_code)             
-    self.where("site_code = ? AND created_date IS NOT NULL 
+    datetime = self.where("site_code = ? AND created_date IS NOT NULL 
       AND updated_date IS NOT NULL",site_code).maximum(:created_at)
+    unless datetime.blank?                                                      
+      #something went wrong - so what this block will do is: give query for a date when
+      #the sync was complete.                                                   
+      sync_complete_date = self.where("created_date IS NOT NULL                   
+      AND updated_date IS NOT NULL AND site_code = ?",site_code).maximum(:created_at)                           
+    end                                                                         
+                                                                                
+    unless sync_complete_date.blank?                                            
+      return sync_complete_date                                                 
+    else                                                                        
+      return self.where("created_date IS NOT NULL 
+        AND updated_date IS NOT NULL AND site_code = ?",site_code).maximum(:created_at)
+    end
   end                                                                           
                                                                                 
   def self.check_for_valid_start_date(site_code)                                      
