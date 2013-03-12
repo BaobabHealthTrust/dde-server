e51e28a194acc5ceab82b2897d5ce4cb1071047a
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-03-12 10:54:09 +0200

    merging - UI

--- app/models/master_syncs.rb
+++ app/models/master_syncs.rb
@@ -1,11 +1,12 @@
 class MasterSyncs < ActiveRecord::Base
-  def self.last_updated_date(site_code)             
+
+  def self.last_updated_datetime(site_code)             
     date = self.where("site_code <> ? AND created_date IS NOT NULL 
-      AND updated_date IS NOT NULL",site_code).minimum(:created_date)
+      AND updated_date IS NOT NULL",site_code).minimum(:created_at)
     return date unless date.blank?  
       
     self.where("site_code = ? AND created_date IS NOT NULL 
-      AND updated_date IS NOT NULL",site_code).maximum(:created_date)
+      AND updated_date IS NOT NULL",site_code).maximum(:created_at)
   end                                                                           
                                                                                 
   def self.check_for_valid_start_date(site_code)                                      
@@ -13,7 +14,7 @@ class MasterSyncs < ActiveRecord::Base
       self.create(:created_date => DateTime.now(),:site_code => site_code)                                
     elsif not(self.where("created_date IS NOT NULL AND updated_date IS NOT NULL AND site_code = ?",site_code).last).blank?        
       date = (self.where("created_date IS NOT NULL AND updated_date 
-        IS NOT NULL AND site_code = ?",site_code)).maximum(:created_date)
+        IS NOT NULL AND site_code = ?",site_code)).maximum(:created_at)
       self.create(:created_date => date, :site_code => site_code)                                
     else
       self.create(:created_date => DateTime.now(),:site_code => site_code)                                
