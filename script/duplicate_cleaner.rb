4e205210faffb14613439e7764ac595c49d2ee74
blessings <blessings.mhango@gmail.com>
2013-03-24 19:29:42 +0200

    method to update national ids without decimal number

--- script/duplicate_cleaner.rb
+++ script/duplicate_cleaner.rb
@@ -1,4 +1,5 @@
 LogErr = Logger.new(Rails.root.join("log","duplicates_error.txt"))
+LogSuc = Logger.new(Rails.root.join("log","success.txt"))
 class DuplicateCleaner
   def self.duplicate_national_ids
     national_id_values = NationalPatientIdentifier.select("value, count(value) as idcount").
@@ -18,7 +19,7 @@ class DuplicateCleaner
             puts "deleted #{national_id.value} >>>>>>>>>>>>>>>>>"
         end
     end
-   
+
     national_ids.each do|national_id|
       if national_id.decimal_num.blank? and national_id.person_id
         national_id.decimal_num = NationalPatientId.to_decimal(national_id.value, 30) / 10
@@ -26,7 +27,33 @@ class DuplicateCleaner
         puts "updated #{national_id.value} <<<<<<<<<<<<<<<<"
       end
     end
-    puts "Started at: #{start} ########## finished at:#{Time.now()}"
+    puts "Started at: #{start.strftime("%Y-%m-%d %H:%M:%S")} ########## finished at:#{Time.now().strftime("%Y-%m-%d %H:%M:%S")}"
   end
- update_national_ids_decimal_num
+
+  def self.assign_national_ids_decimal_num
+    start = Time.now()
+    puts "Started at #{start.strftime("%Y-%m-%d %H:%M:%S")}"
+    national_ids = NationalPatientIdentifier.where("decimal_num IS NULL AND person_id IS NOT NULL").order("value")
+    LogSuc.info "There are  #{national_ids.count} ids that were assigned with decimal num"
+    puts "There are  #{national_ids.count} ids that were assigned without decimal num"
+    updated_national_ids = []
+    national_ids.each do|national_id|
+      if national_id.decimal_num.blank? and national_id.person_id
+        national_id.decimal_num = NationalPatientId.to_decimal(national_id.value, 30) / 10
+        national_id.save! rescue LogErr.info("failed to update >> #{national_id.value} for >> #{national_id.person_id}") and next
+        puts "updated #{national_id.value} <<<<<<<<<<<<<<<<"
+        LogSuc.info "updated #{national_id.value}"
+        updated_national_ids << national_id.value
+      end
+    end
+
+    puts "There were #{national_ids.count} assigned national ids without decimal num"
+    puts "#{updated_national_ids.count} national ids were updated"
+    puts "#{national_ids.count - updated_national_ids.count} national ids were not updated"
+    puts "Started at :#{start.strftime("%Y-%m-%d %H:%M:%S")} ##### finished at :#{Time.now().strftime("%Y-%m-%d %H:%M:%S")}"
+  end
+
+ #update_national_ids_decimal_num
+ assign_national_ids_decimal_num
+
 end
