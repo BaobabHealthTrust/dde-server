bfeeccd9b64b5b7727232e94a2cee2a1524eb7da
blessings <blessings.mhango@gmail.com>
2013-04-30 15:45:25 +0200

    validation

--- app/models/npid_auto_generation.rb
+++ app/models/npid_auto_generation.rb
@@ -1,3 +1,8 @@
 class NpidAutoGeneration < ActiveRecord::Base
   belongs_to :site
+  
+  validates :threshold, :site_id, :presence => :true
+  validates :threshold, :numericality => { :only_integer => true }
+  validates :threshold, :numericality => { :greater_than => 0 }
+
 end
