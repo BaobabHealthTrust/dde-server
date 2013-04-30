fb24b6cc35fe2f115a3593f372f738c408095fd9
blessings <blessings.mhango@gmail.com>
2013-04-30 16:06:33 +0200

    working on master configuration

--- app/controllers/npid_auto_generations_controller.rb
+++ app/controllers/npid_auto_generations_controller.rb
@@ -6,6 +6,11 @@ class NpidAutoGenerationsController < ApplicationController
       npid_autogenerate.site_id = Site.current_id
       npid_autogenerate.threshold = params[:npid_auto_generation][:threshold]
       npid_autogenerate.save!
+    else
+      npid_autogenerate = NpidAutoGeneration.new
+      npid_autogenerate.site_id = params[:npid_auto_generation][:site_id]
+      npid_autogenerate.threshold = params[:npid_auto_generation][:threshold]
+      npid_autogenerate.save!
     end
    redirect_to npid_auto_generations_path
   end
