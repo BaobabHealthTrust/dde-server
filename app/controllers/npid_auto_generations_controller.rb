113540eb2a22380c1f6e9130665f53d4b5c69789
blessings <blessings.mhango@gmail.com>
2013-05-02 12:59:03 +0200

    limiting, editing and updating thresholds

--- app/controllers/npid_auto_generations_controller.rb
+++ app/controllers/npid_auto_generations_controller.rb
@@ -12,15 +12,57 @@ class NpidAutoGenerationsController < ApplicationController
       npid_autogenerate.threshold = params[:npid_auto_generation][:threshold]
       npid_autogenerate.save!
     end
+
    redirect_to npid_auto_generations_path
+   
   end
 
   def index
     @npid_autogenerates = NpidAutoGeneration.page(params[:page]).all
+
+    if Site.master?
+      npid_set_sites = NpidAutoGeneration.count
+      sites = Site.count
+      @unset_sites_count = sites - npid_set_sites
+    else
+      @site_not_set = NpidAutoGeneration.count == 0 ? true : false
+    end
+
   end
 
   def new
     @npid_autogenerate = NpidAutoGeneration.new
+
+    if Site.master?
+      @npid_set_sites = NpidAutoGeneration.all.collect(&:site_id)
+      unless @npid_set_sites.blank?
+        @sites = Site.where("id NOT IN (?)", @npid_set_sites)
+      else
+        @sites = Site.all
+      end
+    end
+    
+  end
+
+  def edit
+    @npid_autogenerate = NpidAutoGeneration.find(params[:id])
+ 
+    if Site.master?
+      @npid_set_sites = NpidAutoGeneration.all.collect(&:site_id)
+      unless @npid_set_sites.blank?
+        @sites = Site.where("id NOT IN (?)", @npid_set_sites)
+      else
+        @sites = Site.all
+      end
+    end
+
+  end
+
+  def update
+    @npid_autogenerate = NpidAutoGeneration.find(params[:id])
+    @npid_autogenerate.threshold = params[:npid_auto_generation][:threshold]
+    @npid_autogenerate.save!
+    redirect_to npid_auto_generations_path
   end
 
   def destroy
