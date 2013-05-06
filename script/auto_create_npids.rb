625498425fe00c5b322ddd35d5e2747fb3217ef9
blessings <blessings.mhango@gmail.com>
2013-05-06 22:13:43 +0200

    refactoring

--- script/auto_create_npids.rb
+++ script/auto_create_npids.rb
@@ -1,22 +1,23 @@
-require 'rubygems'
-require 'rest-client'
-require 'json'
-require 'rails'
-
-Modulepath = File.expand_path($PROGRAM_NAME)
-AppPath = Modulepath.gsub($PROGRAM_NAME,"")
-LogErr = Logger.new(File.join(AppPath,'log/auto_npid.txt'))
-
+begin
+	require 'rubygems'
+	require 'rest-client'
+	require 'json'
+	require 'rails'
+	LogErr = Logger.new("/var/www/dde-server/log/auto_npid.txt")
+rescue => e
+  LogErr.error(e.inspect)
+end
+AppPort = 3002
 class AutoNpidService
 
   def self.auto_create_npids
-  	npid_autogeneration = NpidAutoGenerationsController.new
-    if Site.master?
-     npid_autogeneration.generate_npids
-    else
-     npid_autogeneration.auto_request_npids
-    end
-  end
+   LogErr.info "in class"
+   begin
+  	results = RestClient.get("http://admin:admin@localhost:#{AppPort}/npid_auto_generations/create_npids")
+   rescue => e
+     LogErr.error(e.inspect)
+   end
+ end
 
   LogErr.info("Started at : #{Time.now().strftime('%Y-%m-%d %H:%M:%S')}")
   auto_create_npids
