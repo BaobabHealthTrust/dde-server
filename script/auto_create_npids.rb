d7e92d1d9f8a7da8fb8afa5439af41c4238690bd
blessings <blessings.mhango@gmail.com>
2013-05-06 17:25:36 +0200

    auto create script - error handling

--- script/auto_create_npids.rb
+++ script/auto_create_npids.rb
@@ -1,22 +1,23 @@
 require 'rubygems'
-require 'rest-client'
-require 'json'
 require 'rails'
+require 'npid_requests_controller'
 
-Modulepath = File.expand_path($PROGRAM_NAME)
-AppPath = Modulepath.gsub($PROGRAM_NAME,"")
-LogErr = Logger.new(File.join(AppPath,'log/auto_npid.txt'))
+LogErr = Logger.new('/var/www/dde-auto-proxy/log/auto_npid.txt')
 
 class AutoNpidService
 
   def self.auto_create_npids
+   begin
   	npid_autogeneration = NpidAutoGenerationsController.new
     if Site.master?
      npid_autogeneration.generate_npids
     else
      npid_autogeneration.auto_request_npids
     end
-  end
+   rescue => e
+     LogErr.error(e.inspect)
+   end
+ end
 
   LogErr.info("Started at : #{Time.now().strftime('%Y-%m-%d %H:%M:%S')}")
   auto_create_npids
