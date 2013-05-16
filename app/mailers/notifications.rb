2bcf7d8bc324202f051232fe7fd2a334a320c0a6
Blessings <blessings.mhango@gmail.com>
2013-05-16 19:26:32 +0200

    calling sendgrid

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,10 +1,17 @@
 class Notifications < ActionMailer::Base
-  default :from => "dde.admin@baobabhealth.org"
+	include SendGrid
+  default :from => User.find_by_name("admin").email
+  sendgrid_category :use_subject_lines
+  sendgrid_enable   :ganalytics, :opentrack
+  sendgrid_unique_args :key1 => Time.now, :key2 => Time.now
+
   def notify(user,site_name,subject,body)
+	 sendgrid_category "Welcome"
+	 sendgrid_unique_args :key2 => Time.now, :key3 => Time.now
    @user = user
    @site_name = site_name
-   @subject = subject.title_case
-   @body = body
+   @subject = subject
+   @email_body = body
    mail(:to => @user.email, :subject => @subject)
   end
 end
