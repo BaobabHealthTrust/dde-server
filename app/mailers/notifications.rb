d2a7d62c05c49734b580b3c17fe74390bc9d31bf
Blessings <blessings.mhango@gmail.com>
2013-05-17 10:26:00 +0200

    sending to multiple users

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -6,12 +6,20 @@ class Notifications < ActionMailer::Base
   sendgrid_unique_args :key1 => Time.now, :key2 => Time.now
 
   def notify(user,site_name,subject,body)
-	 sendgrid_category "Welcome"
+	 sendgrid_category "Notification"
 	 sendgrid_unique_args :key2 => Time.now, :key3 => Time.now
+   sendgrid_recipients users_to_notify
    @user = user
    @site_name = site_name
    @subject = subject
    @email_body = body
    mail(:to => @user.email, :subject => @subject)
+  end
+
+  def users_to_notify
+    users = User.where(:disabled => 0, :notify => 1).collect do |user|
+      user.email
+    end
+    return users
   end
 end
