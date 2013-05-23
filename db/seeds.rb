b160b194b496de3c09668e406d5acdbfc036e991
Blessings <blessings.mhango@gmail.com>
2013-05-23 16:48:20 +0200

    refine seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-site = Site.create :name => 'My Local Site', :code => 'MLS'
+site = Site.create :name => 'My Local Site', :code => 'MLS', :annotations => 'MLS'
+user = User.find_by_name('admin')
+user.email = 'dde@baobabhealth.org' unless user.blank?
+user.notify = true
+user.save!
 
 if Site.master?
   #NationalPatientIdentifier.generate!({:count => 19, :assigner_site_id => site.id})
-
-  User.create :name => 'admin', :password => 'admin',:email => 'dde@baobabhealth.org'
+  User.create :name => 'admin', :password => 'admin',:email => 'dde@baobabhealth.org', :notify => 1
   puts "The user for your site uses API key: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx42 and password: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx23"
+else
+	User.create :name => 'admin', :password => 'admin',:email => 'dde@baobabhealth.org', :notify => true
+  puts "Your new user is: admin, password: admin"
 end
-
-User.create :name => 'admin', :password => 'admin',:email => 'dde@baobabhealth.org'
-puts "Your new user is: admin, password: admin"
 
 
