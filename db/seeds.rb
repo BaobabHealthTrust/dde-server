e6b1035b889547125d3dbc68910026f5f4569d49
Blessings <blessings.mhango@gmail.com>
2013-05-24 12:02:45 +0200

    update user condition

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,11 @@
 
 site = Site.create :name => 'My Local Site', :code => 'MLS', :annotations => 'MLS'
 user = User.find_by_name('admin')
-user.email = 'dde@baobabhealth.org' unless user.blank?
-user.notify = true
-user.save!
-
+unless user.blank?
+	user.email = 'dde@baobabhealth.org'
+	user.notify = true
+	user.save!
+end
 if Site.master?
   #NationalPatientIdentifier.generate!({:count => 19, :assigner_site_id => site.id})
   User.create :name => 'admin', :password => 'admin',:email => 'dde@baobabhealth.org', :notify => 1
